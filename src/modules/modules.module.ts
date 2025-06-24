dd73ff5b98ffd18dce35280f8fac9e3b20d7a1c4
adiazt01 <adiaztovar2004@gmail.com>
2025-06-24 12:13:47 -0400

    Add Payments module with resolver, service, and DTOs for payment handling

--- src/modules/modules.module.ts
+++ src/modules/modules.module.ts
@@ -3,8 +3,9 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
+import { PaymentsModule } from './payments/payments.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, ProductsModule, OrdersModule],
+  imports: [UsersModule, AuthModule, ProductsModule, OrdersModule, PaymentsModule],
 })
 export class ModulesModule {}
